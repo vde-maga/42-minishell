53f88d4772124fafa723acd4979b33b9b0999b2b
Vitor Tiago De Magalhaes Marques <vde-maga@c1r12s2.42porto.com>
2025-11-04 15:58:07 +0000

    fix export behaviour to be equal to original bash

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -5,7 +5,9 @@ static int	ft_handle_export_no_equal(t_minishell *msdata, char *arg)
 {
 	if (!ft_is_valid_identifier(arg))
 	{
-		ft_putstr_fd("minishell: export: not a valid identifier\n", 2);
+		ft_putstr_fd("minishell: export: `", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd("': not a valid identifier\n", 2);
 		return (1);
 	}
 	return (ft_set_env_var(&msdata->env_list, arg, NULL));
@@ -24,12 +26,17 @@ static int	ft_handle_export_with_equal(t_minishell *msdata, char *arg)
 		return (1);
 	if (!ft_is_valid_identifier(var_name))
 	{
-		ft_putstr_fd("minishell: export: not a valid identifier\n", 2);
-		return (free(var_name), 1);
+		ft_putstr_fd("minishell: export: `", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd("': not a valid identifier\n", 2);
+		free(var_name);
+		return (1);
 	}
 	value = ft_remove_quotes(equal_sign + 1);
 	result = ft_set_env_var(&msdata->env_list, var_name, value);
-	return (free(value), free(var_name), result);
+	free(value);
+	free(var_name);
+	return (result);
 }
 
 int	ft_builtin_export(t_minishell *msdata, char *arg)
@@ -38,6 +45,14 @@ int	ft_builtin_export(t_minishell *msdata, char *arg)
 		return (1);
 	if (!arg || !*arg)
 		return (ft_print_env_list(msdata));
+	if (arg[0] == '-')
+	{
+		ft_putstr_fd("minishell: export: ", 2);
+		ft_putstr_fd(arg, 2);
+		ft_putstr_fd(": invalid option\n", 2);
+		ft_putstr_fd("export: usage: export [name[=value] ...] or export -p\n", 2);
+		return (2);
+	}
 	if (!ft_strchr(arg, '='))
 		return (ft_handle_export_no_equal(msdata, arg));
 	return (ft_handle_export_with_equal(msdata, arg));
