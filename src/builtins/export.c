9f8b36641d51b5c18616e2917a9e8d209fb0fe8c
Rui Ribeiro <42305006+ruiribeiro04@users.noreply.github.com>
2025-11-07 12:13:07 +0000

    Fixed `export` builtin (now works with env vars)

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -39,21 +39,40 @@ static int	ft_handle_export_with_equal(t_minishell *msdata, char *arg)
 	return (result);
 }
 
-int	ft_builtin_export(t_minishell *msdata, char *arg)
+static int	ft_process_export_arg(t_minishell *msdata, char *arg)
 {
-	if (!msdata)
-		return (1);
-	if (!arg || !*arg)
-		return (ft_print_env_list(msdata));
 	if (arg[0] == '-')
 	{
 		ft_putstr_fd("minishell: export: ", 2);
 		ft_putstr_fd(arg, 2);
 		ft_putstr_fd(": invalid option\n", 2);
-		ft_putstr_fd("export: usage: export [name[=value] ...] or export -p\n", 2);
+		ft_putstr_fd("export: usage: export [name[=value] ...]", 2);
+		ft_putstr_fd(" or export -p\n", 2);
 		return (2);
 	}
 	if (!ft_strchr(arg, '='))
 		return (ft_handle_export_no_equal(msdata, arg));
 	return (ft_handle_export_with_equal(msdata, arg));
+}
+
+int	ft_builtin_export(t_minishell *msdata, char **args)
+{
+	int	status;
+	int	final_status;
+	int	i;
+
+	if (!msdata || !args)
+		return (1);
+	if (!args[1] || !*args[1])
+		return (ft_print_env_list(msdata));
+	final_status = 0;
+	i = 1;
+	while (args[i])
+	{
+		status = ft_process_export_arg(msdata, args[i]);
+		if (status != 0)
+			final_status = status;
+		i++;
+	}
+	return (final_status);
 }
