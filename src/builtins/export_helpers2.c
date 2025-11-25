045b5b0256c0751ed04ef61ed0476a0ba1c15a11
Vitor Tiago De Magalhaes Marques <vde-maga@c1r12s2.42porto.com>
2025-11-25 17:56:59 +0000

    Fixed export builtin behaviour where it would print all the spaces, instead of one

---
+++ src/builtins/export_helpers2.c
@@ -0,0 +1,50 @@
+#include "minishell.h"
+
+int	ft_calculate_allocate_space(char *value)
+{
+	int	i;
+	int	count_allocate_space;
+
+	i = 0;
+	count_allocate_space = 0;
+	while (value[i])
+	{
+		if (value[i] == ' ')
+		{
+			count_allocate_space++;
+			while (value[i] == ' ')
+				i++;
+		}
+		else
+		{
+			count_allocate_space++;
+			i++;
+		}
+	}
+	return (count_allocate_space);
+}
+
+char	*ft_remove_spaces(char *value)
+{
+	int		i;
+	int		j;
+	char	*new_value;
+
+	new_value = malloc(ft_calculate_allocate_space(value) + 1);
+	if (!new_value)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (value[i])
+	{
+		new_value[j++] = value[i];
+		if (value[i] == ' ')
+			while (value[i] == ' ')
+				i++;
+		else
+			i++;
+	}
+	new_value[j] = '\0';
+	free(value);
+	return (new_value);
+}
